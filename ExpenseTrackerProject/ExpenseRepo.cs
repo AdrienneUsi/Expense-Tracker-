cf724648eaf2d119a25291441e2ff28e83e278f1
AdrienneUsi <usiadrienne35@gmail.com>
2024-02-09 09:32:29 -0800

    finish project

--- ExpenseTrackerProject/ExpenseRepo.cs
+++ ExpenseTrackerProject/ExpenseRepo.cs
@@ -27,14 +27,14 @@ namespace ExpenseTrackerProject
 
         public void UpdateExpense(Expense expense)
         {
-            _conn.Execute("UPDATE expenses SET category_name = @category_name, amount = @amount WHERE expense_id = @expense_id",
-             new { category_name = expense.Category_name, amount = expense.Amount, expense_id = expense.Expense_ID });
+            _conn.Execute("UPDATE expenses SET category_name = @category_name, amount = @amount, date_of_expense = @date_of_expense, description = @description, payment_method = @payment_method WHERE expense_id = @expense_id",
+             new { category_name = expense.Category_name, amount = expense.Amount, date_of_expense = expense.Date_Of_Expense, description = expense.Description, payment_method = expense.Payment_Method, expense_id = expense.Expense_ID });
         }
 
         public void InsertExpense(Expense expenseToInsert)
         {
-            _conn.Execute("INSERT INTO expenses (category_name, amount, expense_id) VALUES (@category, @amount, @expense_id);",
-                new { category = expenseToInsert.Category_name, amount = expenseToInsert.Amount, expense_id = expenseToInsert.Expense_ID });
+            _conn.Execute("INSERT INTO expenses (category_name, amount, date_of_expense, description, payment_method, expense_id) VALUES (@category, @amount, @date_of_expense, @description, @payment_method, @expense_id);",
+                new { category = expenseToInsert.Category_name, amount = expenseToInsert.Amount, date_of_expense = expenseToInsert.Date_Of_Expense, description = expenseToInsert.Description, payment_method = expenseToInsert.Payment_Method, expense_id = expenseToInsert.Expense_ID});
 
 
         }
